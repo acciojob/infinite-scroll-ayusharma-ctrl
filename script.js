a7ad08fa0570a0ca292df55996d8bc617a6956f3
Ayush Sharma <117670536+ayusharma-ctrl@users.noreply.github.com>
2023-02-19 13:01:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here!
+var ol = document.getElementById("infi-list");
 
+function append(index){
+	const item = document.createElement("div");
+	item.textContent = `Item ${index}`
+	ol.appendChild(item);
+}
+
+for(let i=1; i<1000; i++){
+	append(i);
+}
